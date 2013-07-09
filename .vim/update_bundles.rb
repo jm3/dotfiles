8a9eb541823799ca3137c287c43cefa52a3ee871
John Manoogian III <jm3@jm3.net>
2013-07-08 19:36:37 -0700

    indentation

--- .vim/update_bundles.rb
+++ .vim/update_bundles.rb
@@ -1,23 +1,23 @@
 #!/usr/bin/env ruby
 
 git_bundles = [
-  "git://github.com/altercation/vim-colors-solarized.git",  # sane, modern colors
-  "git://github.com/ervandew/supertab.git",                 # tab-complete anything
-  "git://github.com/godlygeek/tabular.git",                 # align assignments and arg lists
-  "git://github.com/jelera/vim-gummybears-colorscheme",     # pretty colors
-  "git://github.com/kchmck/vim-coffee-script.git",          # syntax highlighting for coffeescript
-  "git://github.com/pangloss/vim-javascript.git",           # highlighting + syntax for JS
-  "git://github.com/tomtom/tcomment_vim.git",               # comment any language
-  "git://github.com/tpope/vim-endwise.git",                 # complete simple code structues
+  "git://github.com/altercation/vim-colors-solarized.git", # sane, modern colors
+  "git://github.com/ervandew/supertab.git",                # tab-complete anything
+  "git://github.com/godlygeek/tabular.git",                # align assignments and arg lists
+  "git://github.com/jelera/vim-gummybears-colorscheme",    # pretty colors
+  "git://github.com/kchmck/vim-coffee-script.git",         # syntax highlighting for coffeescript
+  "git://github.com/pangloss/vim-javascript.git",          # highlighting + syntax for JS
+  "git://github.com/tomtom/tcomment_vim.git",              # comment any language
+  "git://github.com/tpope/vim-endwise.git",                # complete simple code structues
   "git://github.com/tpope/vim-haml.git",
   "git://github.com/tpope/vim-markdown.git",
   "git://github.com/tpope/vim-rails.git",
-  "git://github.com/tpope/vim-repeat.git",                  # make . repeat the last whole plugin command
+  "git://github.com/tpope/vim-repeat.git",                 # make . repeat the last whole plugin command
   "git://github.com/tpope/vim-vividchalk.git",
   "git://github.com/tsaleh/vim-matchit.git",
   "git://github.com/vim-ruby/vim-ruby.git",
   "git://github.com/vim-scripts/Gist.vim.git",
-  "git://github.com/vim-scripts/mru.vim.git"                # navigate most-recently used files
+  "git://github.com/vim-scripts/mru.vim.git"               # navigate most-recently used files
 ]
 
 # mess with these later:
