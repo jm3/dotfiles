cfcc7e9d0bc0abdc41b254fc1e0e01fb3d503b15
John Manoogian III <jm3@jm3.net>
2013-07-20 13:24:18 -0700

    clean up .js files

--- .js/github.com.js
+++
@@ -1,7 +0,0 @@
-/*
- * swap github logo with trollface
- * $('#header .logo img')
- * .css('width', '100px')
- * .css('margin-top', '-15px')
- * .attr('src', '//bit.ly/ghD24e')
- */
--- .js/www.youtube.com.js
+++ .js/www.youtube.com.js
@@ -1,3 +1,2 @@
 // hide comments (but keep comment counter)
-console.log( "IN HERE" );
 setTimeout( function() { $(".comment-list").hide(); }, 1000 );
