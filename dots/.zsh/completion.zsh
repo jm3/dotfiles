de17bc6dc15166b2a6abdb45ea78c60100c0c460
John Manoogian III <jm3@jm3.net>
2013-08-01 19:07:27 -0700

    improve hostname completion

--- dots/.zsh/completion.zsh
+++ dots/.zsh/completion.zsh
@@ -4,10 +4,21 @@ autoload -U compinit && compinit -u # rm -f ~/.zcompdump if rvm complains
 # complete active screens:
 compctl -g "/tmp/uscreens/S-${USER}/*(p:t)" screen
 
-# complete on hostnames:
+# by default, ssh completes usernames with every user found in
+# /etc/passwd, which means that when using zsh's default completion,
+# ~30 useless auto-created osx user accounts appear for completion
+#
+# this fixes that.
+zstyle ':completion:*' users $USER root ubuntu
+
+# complete hostnames sanely.
+# by default, ssh completes hostnames found in ssh/known_hosts.
+# here we add a few to that, while excluding IP addresses.
 h=140proof.com
-myhosts=($h 280.$h web1.$h api1.$h neo4j1.$h graphite1.$h google.com)
-compctl -k myhosts ping lynx whois ssh telnet ftp host nslookup
+known_hosts=`cat ~/.ssh/known_hosts | cut -f1 -d \ | sed "s/,.*//" | egrep -v "\d+\.\d+\.\d+\.\d+"`
+echo $known_hosts
+explicit_hosts=($known_hosts $h 280.$h web1.$h api1.$h neo4j1.$h graphite1.$h google.com pubapi1.$h)
+zstyle ':completion:*' hosts $known_hosts
 
 # complete common bundle commands
 bundles=(exec install outdated package show update)
