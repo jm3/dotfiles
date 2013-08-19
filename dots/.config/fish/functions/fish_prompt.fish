1c8cc04d5b8bc206b501c43bb5e090971db8e856
John Manoogian III <jm3@jm3.net>
2013-08-19 20:24:11 +0000

    make fish prompt back-compatible with 1.23

--- dots/.config/fish/functions/fish_prompt.fish
+++ dots/.config/fish/functions/fish_prompt.fish
@@ -1,13 +1,20 @@
 function fish_prompt
-  if not set -q -g __fish_robbyrussell_functions_defined
-    set -g __fish_robbyrussell_functions_defined
-    function _git_branch_name
-      echo (git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
-    end
 
-    function _is_git_dirty
-      echo (git status -s --ignore-submodules=dirty ^/dev/null)
-    end
+  # darwin sed uses "-E", while ubuntu sed uses "-r"
+  set -l os (uname)
+  # convert full version string 1.25.6 => 1
+  if [ $os = "Darwin" ]
+    set major_version (echo $version | sed -E "s/([0-9])+\..*/\1/")
+  else
+    set major_version (echo $version | sed -r "s/([0-9])+\..*/\1/")
+  end
+
+  function _git_branch_name
+    echo (git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+  end
+
+  function _is_git_dirty
+    echo (git status -s --ignore-submodules=dirty ^/dev/null)
   end
 
   set -l cyan (set_color -o cyan)
@@ -29,5 +36,11 @@ function fish_prompt
     end
   end
 
-  echo -n -s $arrow $cwd $git_info $normal '> '
+  # "echo -s" is a fish 2.0+ only feature
+  if test $major_version -lt 2
+    echo -n $arrow $cwd $git_info $normal '> '
+  else
+    echo -n -s $arrow $cwd $git_info $normal '> '
+  end
+
 end
