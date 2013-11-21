1ac915967895527d3a56d8c1d116026499f8c41d
John Manoogian III <jm3@jm3.net>
2013-11-20 17:48:50 -0800

    zsh: load all .PEMs and ssh keys

--- dots/.zsh/load-ssh-keys.zsh
+++ dots/.zsh/load-ssh-keys.zsh
@@ -1,9 +1,8 @@
 # if we haven't already, load any keys so all apps can find them
 num_keys_loaded=$(ssh-add -l | grep -v "no identities" | wc -l)
 if [ $num_keys_loaded -lt 1 ]; then
-  for key in $(find ~/.ssh/ -name '*sa'); do
-    eval `ssh-agent` &> /dev/null && ssh-add ~/.ssh/*sa &> /dev/null
-    break
-  done
+  eval `ssh-agent` &> /dev/null
+  ssh-add ~/.ssh/*pem &> /dev/null
+  ssh-add ~/.ssh/*sa &> /dev/null
 fi
 
