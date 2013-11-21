4b15eae727b685f3987a689c127178e70cf42ab9
John Manoogian III <jm3@jm3.net>
2013-11-20 18:01:43 -0800

    fix ssh-agent laoder so only one can run

--- dots/.zsh/load-ssh-keys.zsh
+++ dots/.zsh/load-ssh-keys.zsh
@@ -1,8 +1,11 @@
-# if we haven't already, load any keys so all apps can find them
-num_keys_loaded=$(ssh-add -l | grep -v "no identities" | wc -l)
-if [ $num_keys_loaded -lt 1 ]; then
-  eval `ssh-agent` &> /dev/null
-  ssh-add ~/.ssh/*pem &> /dev/null
-  ssh-add ~/.ssh/*sa &> /dev/null
+agents_running=`ps aux | grep "ssh-agen[t]" | wc -l | awk '{print $1}'`
+if [ $agents_running -gt 0 ]; then
+  # ok, at least one ssh-agent is running.
+else
+    echo "ssh agent not already running; starting a new one"
+    eval `ssh-agent -s`
 fi
+
+ssh-add ~/.ssh/*pem &> /dev/null
+ssh-add ~/.ssh/*sa &> /dev/null
 
