b00221f32b687f514b9576ace7fc573138d49414
John Manoogian III <jm3@jm3.net>
2013-11-20 23:59:28 -0800

    zsh: shut up about ssh agents

--- dots/.zsh/load-ssh-keys.zsh
+++ dots/.zsh/load-ssh-keys.zsh
@@ -2,8 +2,8 @@ agents_running=`ps aux | grep "ssh-agen[t]" | wc -l | awk '{print $1}'`
 if [ $agents_running -gt 0 ]; then
   # ok, at least one ssh-agent is running.
 else
-    echo "ssh agent not already running; starting a new one"
-    eval `ssh-agent -s`
+    # ssh agent not already running; start a new one
+    eval `ssh-agent -s` > /dev/null
 fi
 
 keys_to_add=`find ~/.ssh -name \*pem -o -name \*sa`
