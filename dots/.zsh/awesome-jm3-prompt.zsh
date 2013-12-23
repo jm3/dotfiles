eec80bbee9f7c484e585d0b7a3541d8b1cf06ebc
John Manoogian III <jm3@140proof.com>
2013-12-23 14:04:59 -0700

    add dumb term support to prompt

--- dots/.zsh/awesome-jm3-prompt.zsh
+++ dots/.zsh/awesome-jm3-prompt.zsh
@@ -3,27 +3,31 @@
 # https://raw.github.com/jm3/dotfiles/master/prompt.gif
 
 awesome_jm3_prompt() {
-  arrow="%{$fg[red]%}➜"
-  cwd="%{$fg[cyan]%}%B%3c%(#.#.)%b"
+  if [ "$TERM" = "dumb" ]; then
+    echo -n "# > "
+  else
+    arrow="%{$fg[red]%}➜"
+    cwd="%{$fg[cyan]%}%B%3c%(#.#.)%b"
 
-  git_info() {
-    is_git_dirty() {
-      git status --porcelain | egrep "\sM|\sD|\?\?" &> /dev/null
-    }
-    branch=`git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* //' `
-    if [ ! -z "${branch}" ];
-    then
-      is_git_dirty
-      if [ $? -eq 0 ];
+    git_info() {
+      is_git_dirty() {
+        git status --porcelain | egrep "\sM|\sD|\?\?" &> /dev/null
+      }
+      branch=`git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* //' `
+      if [ ! -z "${branch}" ];
       then
-        git_filth=" %{$fg[yellow]%}✗%{$reset_color%}"
+        is_git_dirty
+        if [ $? -eq 0 ];
+        then
+          git_filth=" %{$fg[yellow]%}✗%{$reset_color%}"
+        fi
+        git_branch=" (%{$fg[red]%}${branch}%{$reset_color%})"
+        echo -n "${git_branch}${git_filth}"
       fi
-      git_branch=" (%{$fg[red]%}${branch}%{$reset_color%})"
-      echo -n "${git_branch}${git_filth}"
-    fi
-  }
+    }
 
-  echo -n "${arrow} ${cwd}$( git_info) > "
+    echo -n "${arrow} ${cwd}$( git_info) > "
+  fi
 }
 
 export PROMPT='$(awesome_jm3_prompt)'
