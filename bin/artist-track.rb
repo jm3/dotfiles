8dbaf01a8bb03846611df467ee790ef9ee5ec94d
John Manoogian III <jm3@jm3.net>
2014-08-06 15:58:22 -0700

    mp3 filename normalizer

---
+++ bin/artist-track.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+require 'mp3info'
+require 'fileutils'
+
+dir = ARGV[0]
+unless dir
+  puts "artist-album DIRECTORY"
+  exit 1
+end
+FileUtils.cd dir
+
+Dir.entries(dir).each do |file|
+   next if file !~ /.mp3$/ # skip files not ending with .mp3
+   f = dir + "/" + file
+   Mp3Info.open(f) do |mp3|      
+     new_name = "#{mp3.tag.artist} - #{mp3.tag.title}.mp3"
+     if file != new_name
+       puts new_name
+       begin
+         FileUtils.mv f, new_name
+       rescue
+       end
+     end
+   end
+end
+
