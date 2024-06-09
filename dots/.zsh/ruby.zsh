b6c2fc2a87210f4ba6db9fda6ad5f85fef6b89fe
John Manoogian III <jm3@jm3.net>
2024-06-09 15:06:17 -0700

    modern chruby setup

--- dots/.zsh/ruby.zsh
+++ dots/.zsh/ruby.zsh
@@ -1,2 +1,8 @@
-source /usr/local/share/chruby/chruby.sh
-chruby 3.0.2
+# source /usr/local/share/chruby/chruby.sh
+source /opt/homebrew/opt/chruby/share/chruby/chruby.sh
+
+# To enable auto-switching of Rubies specified by .ruby-version files,
+# add the following to ~/.bash_profile or ~/.zshrc:
+source /opt/homebrew/opt/chruby/share/chruby/auto.sh
+
+# chruby 3.0.2
