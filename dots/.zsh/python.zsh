7181d201afa18e842ca6a8a07069605c6f30b0a3
jm3 <jm3@jm3.net>
2025-03-13 18:40:27 -0700

    python env

---
+++ dots/.zsh/python.zsh
@@ -0,0 +1,10 @@
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - zsh)"
+
+export LDFLAGS="$LDFLAGS -L/usr/local/opt/zlib/lib"
+export CPPFLAGS="$CPPFLAGS -I/usr/local/opt/zlib/include"
+
+# For pkg-config to find zlib you may need to set:
+export PKG_CONFIG_PATH="/usr/local/opt/zlib/lib/pkgconfig"
+
