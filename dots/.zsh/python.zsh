1ec373435413b3c30d4a0e5ba4e3672ddd1b0217
jm3 <jm3@jm3.net>
2025-03-14 08:00:41 -0700

    ensure pyenv

--- dots/.zsh/python.zsh
+++ dots/.zsh/python.zsh
@@ -1,3 +1,4 @@
+# brew install pyenv
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init - zsh)"
@@ -7,4 +8,3 @@ export CPPFLAGS="$CPPFLAGS -I/usr/local/opt/zlib/include"
 
 # For pkg-config to find zlib you may need to set:
 export PKG_CONFIG_PATH="/usr/local/opt/zlib/lib/pkgconfig"
-
