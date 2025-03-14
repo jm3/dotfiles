6d63722d3d34597b847279b1fc6c7dff8e630f20
jm3 <jm3@jm3.net>
2025-03-13 18:54:25 -0700

    ruby setup

--- dots/.zsh/ruby.zsh
+++ dots/.zsh/ruby.zsh
@@ -1,8 +1,2 @@
-# source /usr/local/share/chruby/chruby.sh
-source /opt/homebrew/opt/chruby/share/chruby/chruby.sh
-
-# To enable auto-switching of Rubies specified by .ruby-version files,
-# add the following to ~/.bash_profile or ~/.zshrc:
-source /opt/homebrew/opt/chruby/share/chruby/auto.sh
-
-# chruby 3.0.2
+# prev. i used this to set up rvm or chruby, but now i'm on rbenv which
+# i don't think needs any setup
