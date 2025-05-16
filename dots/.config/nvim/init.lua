ff7a38dec9e56574c04bc8480c1fcf6ccea7145f
jm3 <jm3@jm3.net>
2025-05-15 20:32:10 -0700

    neovim init: add paths for gf rapid file open

--- dots/.config/nvim/init.lua
+++ dots/.config/nvim/init.lua
@@ -3,13 +3,13 @@
 -- put cursor on any line below and press gF (buffer local shortcut)
 -- to open one of the Lua config files below
 require("lazy_bootstrap").setup()
-require("lazy").setup(require("plugins"))
+require("lazy").setup(require("plugins")) -- ~/.config/nvim/lua/plugins.lua
 
 -- require("plugins") - -included only for easy access to plugin configs via gF :)
 require("core")
 require("code")
 require("keys")
-pcall(require, "personal") -- insert personal configs here (optional)
+pcall(require, "personal") -- ~/.config/nvim/lua/personal.lua
 require("spacing")
 require("spell_autocorrects")
 require("super_powers")
